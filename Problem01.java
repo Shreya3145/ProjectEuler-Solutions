13bc182edfa955afacd964e46b34567db54a84eb
Shreya Ravichandiran <sudhashreya07@gmail.com>
2025-03-09 20:21:53 +0530

    Multiples of 3 or 5
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.

--- Problem01.java
+++ Problem01.java
@@ -3,11 +3,11 @@ public class Problem01 {
         int n = 1000;
         int[] k = new int[n]; 
         int index = 0; 
-        int s=0;
+        int s = 0;
         for (int i = 1; i < n; i++) {
-            if (i%3==0 || i%5==0) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 k[index] = i;   
-                s+=k[index];
+                s += k[index];
                 System.out.println(s); 
                 index++; 
             }
