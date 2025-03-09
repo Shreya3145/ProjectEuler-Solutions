396745a0e0e26f46aad69b4bf23e57107a7cc92e
Shreya Ravichandiran <sudhashreya07@gmail.com>
2025-03-09 20:22:23 +0530

    Even Fibonacci Numbers
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

--- Problem02.java
+++ Problem02.java
@@ -1,17 +1,17 @@
 public class Problem02 {
 public static void main(String args[]){
-    int n1=1, n2=1, n3=0, limit=5000000, sum=0;
-    while(true){
-        n3=n1+n2; 
-        n1=n2; 
-        n2=n3;
-        if(n3%2==0){
+    int n1=1, n2=1, n3=0, limit=5000000, sum=0, ans=0;
+
+    while(n3 <= limit){
+        n3 = n1 + n2; 
+        n1 = n2; 
+        n2 = n3;
+        if(n3 % 2 == 0){
             sum += n3;
-            if(sum<=limit)
-            System.out.println(sum);
-            else
-            break;
+            ans = (sum <= limit) ? sum : limit;
+            
         }
     }
+    System.out.println(ans);
 }
 }
